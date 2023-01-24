db7d201d73e7786b174e53e7d784812fcdef025a
Jon Heaney <113617821+Jonathlon@users.noreply.github.com>
2023-01-24 19:43:42 +0000

    adding loop with height

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
 //List of Pokemon Characters
 let pokemonList = [ 
-    { name:'Pikachu', height: 1.04, type: 'electric' },
-    { name:'Bulbasaur', height: 7, type: ['grass', 'poison'] },
-    { name:'Squirtle', height: 1.08, type: 'water' },
-    { name:'Beedrill', height: 3.03, type: ['bug', 'poison'] },
+    { name: "Pikachu", height: 1.04, type: 'electric' },
+    { name: "Bulbasaur", height: 2.04, type: ['grass', 'poison'] },
+    { name: "Squirtle", height: 1.08, type: 'water' },
+    { name: "Beedrill", height: 3.03, type: ['bug', 'poison'] },
+    { name: "Dragonite", height: 7.03, type: ['dragon', 'flying']}
 ]
 
+for (let i = 0; i < pokemonList.length; i++) {        
+    if (pokemonList[i].height >= 7) {
+      document.write(pokemonList[i].name + "(height:" + pokemonList[i].height + ") - Wow! that is a big pokemon!" + "<br>" );
+    } else if (pokemonList[i].height){
+      document.write(pokemonList[i].name + "(height:" + pokemonList[i].height + ")  " + "<br>") 
+    }
+  }
