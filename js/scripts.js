ae14a8a32a7edf694de83b5104642c9f784409df
Jon Heaney <113617821+Jonathlon@users.noreply.github.com>
2023-01-24 11:58:34 +0000

    added list of Pokemon characters

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
-alert('Hello world');
+//List of Pokemon Characters
+let pokemonList = [ 
+    { name:'Pikachu', height: 1.04, type: 'electric' },
+    { name:'Bulbasaur', height: 7, type: ['grass', 'poison'] },
+    { name:'Squirtle', height: 1.08, type: 'water' },
+    { name:'Beedrill', height: 3.03, type: ['bug', 'poison'] },
+]
 
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
