55cb2f9a32758d1bc4616a9f0f858847031090fd
Jon Heaney <113617821+Jonathlon@users.noreply.github.com>
2023-01-28 16:27:27 +0000

    added some css styling to pokeapp

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,15 @@ let pokemonList = [
     { name: "Bulbasaur", height: 2.04, type: ['grass', 'poison'] },
     { name: "Squirtle", height: 1.08, type: 'water' },
     { name: "Beedrill", height: 3.03, type: ['bug', 'poison'] },
-    { name: "Dragonite", height: 7.03, type: ['dragon', 'flying']}
+    { name: "Dragonite", height: 7.03, type: ['dragon', 'flying']},
+    { name: "Igglybuff", height: 1.01, type: ['Normal', 'Fairy']}
 ]
 
+//loop displaying Pokemon Characters 
 for (let i = 0; i < pokemonList.length; i++) {        
     if (pokemonList[i].height >= 7) {
-      document.write(pokemonList[i].name + "(height:" + pokemonList[i].height + ") - Wow! that is a big pokemon!" + "<br>" );
+      document.write('<div class="card">' + '<p>' + pokemonList[i].name + " " + "(Height:" + pokemonList[i].height + ") - Wow! that is a big pokemon!" +  '</p>' + '</div>');
     } else if (pokemonList[i].height){
-      document.write(pokemonList[i].name + "(height:" + pokemonList[i].height + ")  " + "<br>") 
+      document.write("<div class='card'>" + "<p>" + pokemonList[i].name + " " + "(Height:" + pokemonList[i].height + ")  " + "</p>" + "</div>") 
     }
   }
