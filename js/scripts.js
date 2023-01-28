f82dbc416b86fb7f0db6f8381c9f983d1c19f88b
Jon Heaney <113617821+Jonathlon@users.noreply.github.com>
2023-01-28 17:50:16 +0000

    more css - inc mobile

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,14 @@ let pokemonList = [
     { name: "Squirtle", height: 1.08, type: 'water' },
     { name: "Beedrill", height: 3.03, type: ['bug', 'poison'] },
     { name: "Dragonite", height: 7.03, type: ['dragon', 'flying']},
-    { name: "Igglybuff", height: 1.01, type: ['Normal', 'Fairy']}
+    { name: "Igglybuff", height: 1.01, type: ['Normal', 'Fairy']},    
 ]
 
 //loop displaying Pokemon Characters 
 for (let i = 0; i < pokemonList.length; i++) {        
     if (pokemonList[i].height >= 7) {
-      document.write('<div class="card">' + '<p>' + pokemonList[i].name + " " + "(Height:" + pokemonList[i].height + ") - Wow! that is a big pokemon!" +  '</p>' + '</div>');
+      document.write('<div class="card">' + '<p>' + pokemonList[i].name + " " + "(Height:" + " " + pokemonList[i].height + ") - Wow! that is a big pokemon!" +  '</p>' + '</div>');
     } else if (pokemonList[i].height){
-      document.write("<div class='card'>" + "<p>" + pokemonList[i].name + " " + "(Height:" + pokemonList[i].height + ")  " + "</p>" + "</div>") 
+      document.write("<div class='card'>" + "<p>" + pokemonList[i].name + " " + "(Height:" + " " + pokemonList[i].height + ")  " + "</p>" + "</div>") 
     }
   }
